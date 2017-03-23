36013360d944e464c0e6ee47b1b73b13a249806c
uccidibuti <ucci.dibuti@gmail.com>
2017-03-23 10:42:09 +0100

    Upgrade Example

--- example_index.cpp
+++ example_index.cpp
@@ -69,7 +69,7 @@ int main(){
 	result_index = OZBCIndex[to_search].toVector();
 	gettimeofday(&t2,NULL);
 	time_diff = elapsed_time(&t1,&t2);
-	printf("OZBCIndex make query in: %u microseconds on %u values\n",time_diff);
+	printf("OZBCIndex make query in: %u microseconds\n",time_diff);
 	printf("-----------------------------------\n");
 
 	gettimeofday(&t1,NULL);
